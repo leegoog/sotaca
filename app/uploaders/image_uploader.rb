28600bd0c45330b28cf356d91ede019167ad7aa9
Thomas <thomasmaximini@gmx.de>
2011-05-14 18:53:32 +0200

    devise working now and design of product list finished

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,12 +17,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
+  #def default_url
+  #  "/images/fallback/"+version_name+"/dummy.jpg"
+  #end
+ 
   # Process files as they are uploaded:
-   process :resize_to_fit => [800, 800]
+  # process :resize_to_fit => [550, 550]
   #
   # def scale(width, height)
   #   # do something
@@ -30,7 +30,10 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
    version :thumb do
-     process :resize_to_limit => [200, 200]
+     process :resize_to_limit => [200, 150]
+   end
+   version :big do
+     process :resize_to_limit => [550, 550]
    end
 
   # Add a white list of extensions which are allowed to be uploaded.
--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -1,7 +1,24 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Asset do
-  it "should be valid" do
-    Asset.new.should be_valid
+
+
+  def new_asset(attributes = {})
+    attributes[:product_id] ||= '1'
+    Asset.new(attributes)
   end
+
+  before(:each) do
+    Asset.delete_all
+  end
+  
+  it "should be valid" do
+    new_asset.should be_valid
+  end
+  
+  it "should be related to a product" do
+    new_asset(:product_id => '').should_not be_valid
+  end
+  
+  
 end
