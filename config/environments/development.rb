aa2a90ff780ef2b35b3eecf99184b29ad9abc26a
Thomas <thomasmaximini@gmx.de>
2011-06-30 23:29:40 +0200

    switched to uk paypal sandbox

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,9 +29,9 @@ Sotaca::Application.configure do
   config.after_initialize do
     ActiveMerchant::Billing::Base.mode = :test
     ::GATEWAY = ActiveMerchant::Billing::PaypalGateway.new(
-      :login => "seller_1309100742_biz_api1.trianglecollective.com",
-      :password => "1309100780",
-      :signature => "AZcrOntOepYO1-CTOQW37j.VmTudAiegMp10HrV4.MygIgob0GkpuJ42"
+      :login => "seller_1309468633_biz_api1.sotaca1898.com",
+      :password => "1309468677",
+      :signature => "A2TmxPHrKbxkYzrV0vR-LLSlaaL7A3nJ6XG47NViW-7kPA5zro5L-vSX "
     )
   end
   
