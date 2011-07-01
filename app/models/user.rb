20431a44df79e212c34877e84e43646a388b2562
Thomas <thomasmaximini@gmx.de>
2011-07-01 17:08:50 +0200

    CNY <-> GBP convertion. chinese locales added

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User < ActiveRecord::Base
   attr_accessible :username, :email, :password, :password_confirmation, :login
 
   
+  has_many :carts
+  
   has_and_belongs_to_many :roles
 
   validates_presence_of :username
