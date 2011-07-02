53a3e791ec301f6bc673dd449537a37aac445a90
Thomas <thomasmaximini@gmx.de>
2011-07-02 16:18:03 +0200

    devise mailer

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {  
+  :address              => "mail.sotaca1898.com",  
+  :port                 => 26,  
+  :domain               => "sotaca1898.com",  
+  :user_name            => "no-reply+sotaca1898.com",  
+  :password             => "Triangle.Sotaca",  
+  :authentication       => "plain",  
+  :enable_starttls_auto => true  
+}
