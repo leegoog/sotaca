19648bfacc43e0151898cd12b42bc1da73139c05
Thomas <thomasmaximini@gmx.de>
2011-07-02 17:44:54 +0200

    hostgator mailer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
+  
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
@@ -23,6 +23,8 @@ Devise.setup do |config|
   # You can also supply a hash where the value is a boolean determining whether
   # or not authentication should be aborted when the value is not present.
    config.authentication_keys = [ :login ]
+
+   config.mailer_sender = "no-reply@sotaca1898.com"
 
   # Configure parameters from the request object used for authentication. Each entry
   # given should be a request method and it will automatically be passed to the
