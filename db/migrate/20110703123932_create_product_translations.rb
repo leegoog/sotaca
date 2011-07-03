876b8bf5c847199943b2f254cb054abebd4a1c70
Thomas <thomasmaximini@gmx.de>
2011-07-03 16:08:09 +0200

    globalize3 and batch translations

---
+++ db/migrate/20110703123932_create_product_translations.rb
@@ -0,0 +1,14 @@
+class CreateProductTranslations < ActiveRecord::Migration
+  def self.up
+        Product.create_translation_table!({
+          :title => :string,
+          :subtitle => :string,
+          :description => :text
+        }, {
+          :migrate_data => true
+        })
+      end
+      def self.down
+        Product.drop_translation_table! :migrate_data => true
+      end
+end
