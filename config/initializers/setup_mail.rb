be6bdf277c25f12d0171b581d6690952b283941c
Thomas <thomasmaximini@gmx.de>
2011-07-06 11:03:03 +0200

    mailaccount temp. switched

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,19 +1,19 @@
-ActionMailer::Base.smtp_settings = {  
-  :address              => "mail.sotaca1898.com",  
-  :port                 => 26,  
-  :domain               => "sotaca1898.com",  
-  :user_name            => "no-reply+sotaca1898.com",  
-  :password             => "Triangle.Sotaca",  
-  :authentication       => "plain",  
-}
-
-
 #ActionMailer::Base.smtp_settings = {  
-#  :address              => "smtp.gmail.com",  
-#  :port                 => 587,  
-#  :domain               => "gmail.com",  
-#  :user_name            => "tmaximini@gmail.com",  
-#  :password             => "sum3mer05",  
+#  :address              => "mail.sotaca1898.com",  
+#  :port                 => 26,  
+#  :domain               => "sotaca1898.com",  
+#  :user_name            => "no-reply+sotaca1898.com",  
+#  :password             => "Triangle.Sotaca",  
 #  :authentication       => "plain",  
-#  :enable_starttls_auto => true  
 #}
+#
+
+ActionMailer::Base.smtp_settings = {  
+  :address              => "smtp.gmail.com",  
+  :port                 => 587,  
+  :domain               => "gmail.com",  
+  :user_name            => "tmaximini@gmail.com",  
+  :password             => "sum3mer05",  
+  :authentication       => "plain",  
+  :enable_starttls_auto => true  
+}
