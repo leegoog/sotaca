5d93bb48f71d7c3f49cb38e97e30329a1748ed03
Thomas <thomasmaximini@gmx.de>
2011-07-12 22:18:10 +0200

    redcarpet for line breaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,11 @@ module ApplicationHelper
   def get_cart
     @cart = current_cart
   end
+  
+  def markdown(text)  
+    options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis]  
+    Redcarpet.new(text, *options).to_html.html_safe  
+  end
 
 
 end
