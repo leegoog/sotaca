c04e7c3bfc666b65296d07782e5433408799c32b
Thomas <thomasmaximini@gmx.de>
2011-07-12 21:41:08 +0200

    dummy fallback image, confirmation removed from link_to_remove (error, why?)

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -18,9 +18,15 @@ class LineItem < ActiveRecord::Base
     self.quantity += 1
   end
   
+  
+  # the unit_price is dynamically mapped to an Money object (unit_price is in GBP cents integer)
+  # 
   composed_of :unit_price,
                 :class_name => 'Money',
+                # map unit_price to Money.new.cents
                 :mapping => %w(unit_price cents),
+                # if the unit_price value responds to the 'to_money' method, which it should, its initialized
+                # else we get an empty Money object
                 :converter => Proc.new { |value| value.respond_to?(:to_money) ? value.to_money : Money.empty }
   
 end
