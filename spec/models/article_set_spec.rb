8ebdf4c89402e700244052432994d842fc7c92ce
Thomas <thomasmaximini@gmx.de>
2011-07-13 18:28:48 +0200

    session path

--- spec/models/article_set_spec.rb
+++ spec/models/article_set_spec.rb
@@ -4,4 +4,14 @@ describe ArticleSet do
   it "should be valid" do
     ArticleSet.new.should be_valid
   end
+  
+  context "with 2 or more comments" do
+    it "orders them in reverse" do
+      set = ArticleSet.create
+      comment1 = post.comment("first")
+      comment2 = post.comment("second")
+      set.reload.comments.should eq([comment2, comment1])
+    end
+  end
+  
 end
