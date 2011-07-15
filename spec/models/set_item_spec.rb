f9796a86b67f70e8a9af8760fdfb40f4c768935a
Thomas <thomasmaximini@gmx.de>
2011-07-15 17:52:15 +0200

    blog images now created as png

--- spec/models/set_item_spec.rb
+++ spec/models/set_item_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe SetItem do
-  pending "add some examples to (or delete) #{__FILE__}"
+  pending "add some examples to (or delete) #{__FILE__} Yo"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,52 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  
+  def valid_user_attributes
+    {
+      :username => "alice",
+      :email => "alice@example.com",
+      :password => "secret",
+      :password_confirmation => "secret"
+    }
+  end
+  
+  context "validitions" do
+    
+    let(:alice) { User.new(valid_user_attributes)}
+    
+    it "has a valid username" do
+      alice.should be_valid
+    end
+    
+    it "has an valid email adress" do
+      alice.should be_valid
+    end
+    
+    it "does not accept an invalid email" do
+      alice.email = "@de.ca."
+      alice.should_not be_valid
+      alice.should have(2).error_on(:email)
+      alice.errors.on(:email).should include("is invalid")
+    end
+    
+    it "should check that password and confirmation match" do
+      alice.password_confirmation = "s3cr3t"
+      alice.should_not be_valid
+    end
+    
+    it "should use unique usernames" do
+      alice = User.create!(valid_user_attributes)
+      alice2 = User.new(valid_user_attributes)
+      alice2.should_not be_valid
+      alice2.should have(1).error_on(:username)
+      alice2.should have(2).error_on(:email)
+      alice2.errors.on(:username).should include("already taken")
+    end
+    
+
+    
+  end
+  
 end
