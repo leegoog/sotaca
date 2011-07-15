f403f2cc1a1004266622dbc4037580f6c633ec25
Thomas <thomasmaximini@gmx.de>
2011-07-15 17:52:15 +0200

    blog images now created as png

--- spec/models/article_set_spec.rb
+++ spec/models/article_set_spec.rb
@@ -1,6 +1,13 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe ArticleSet do
+  
+  before(:each) do
+    set = Factory(:article_set)
+  end
+  
+  
+  
   it "should be valid" do
     ArticleSet.new.should be_valid
   end
@@ -8,8 +15,8 @@ describe ArticleSet do
   context "with 2 or more comments" do
     it "orders them in reverse" do
       set = ArticleSet.create
-      comment1 = post.comment("first")
-      comment2 = post.comment("second")
+      comment1 = set.comment("first")
+      comment2 = set.comment("second")
       set.reload.comments.should eq([comment2, comment1])
     end
   end
