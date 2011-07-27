20c34a9e417351ff5bd32efe18442428046b1a4c
Thomas <thomasmaximini@gmx.de>
2011-07-27 21:37:09 +0200

    z index for image gen

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,9 +1,17 @@
 class LineItemsController < ApplicationController
-  def create
+   def create
      @product = Product.find(params[:product_id])
      @cart = current_cart
      @cart.add_item(@product.id, @product.price)
      flash[:notice] = "Added #{@product.title} to cart."
      redirect_to current_cart_url
+   end
+   
+   def destroy
+     @line_item = LineItem.find(params[:id])
+     @cart = @line_item.cart
+     @line_item.destroy
+     flash[:notice] = "Removed from cart."
+     redirect_to @cart
    end
 end
