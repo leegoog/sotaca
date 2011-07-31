d826037ee6630685e4aeea91795f5547af05eef6
Thomas <thomasmaximini@gmx.de>
2011-07-31 22:35:54 +0200

    shipping methods

--- app/models/shipping_method.rb
+++ app/models/shipping_method.rb
@@ -1,3 +1,19 @@
 class ShippingMethod < ActiveRecord::Base
     attr_accessible :name, :price
+    
+    attr_accessor :name_and_price 
+    
+    belongs_to :order
+    
+    composed_of :price,
+                  :class_name => 'Money',
+                  :mapping => %w(price cents),
+                  :converter => Proc.new { |value| value.respond_to?(:to_money) ? value.to_money : Money.empty }
+                  
+    
+    def name_and_price
+      "#{self.name} (#{self.price.format(:symbol => true)})"
+    end
+    
+    
 end
