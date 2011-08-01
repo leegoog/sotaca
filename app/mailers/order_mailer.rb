15c9800df832c48145eb264547041e968905b1c3
Thomas <thomasmaximini@gmx.de>
2011-08-02 00:29:01 +0200

    order number, email confirmations

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,6 +2,7 @@ class OrderMailer < ActionMailer::Base
   default :from => "info@sotaca1898.com"  
 
     def order_confirmation(order)  
+      @order = order
       mail(:to => order.user.email, :subject => "Order Confirmation - #{order.order_nr}")  
     end
 end
