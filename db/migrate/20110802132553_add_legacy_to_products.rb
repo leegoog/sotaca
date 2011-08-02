a685b2afcb058ad3525e6d7ba7e5bce0af89b15a
Thomas <thomasmaximini@gmx.de>
2011-08-02 23:03:48 +0200

    fixed bugs, double validations removed

---
+++ db/migrate/20110802132553_add_legacy_to_products.rb
@@ -0,0 +1,9 @@
+class AddLegacyToProducts < ActiveRecord::Migration
+  def self.up
+    add_column :products, :legacy, :boolean
+  end
+
+  def self.down
+    remove_column :products, :legacy
+  end
+end
