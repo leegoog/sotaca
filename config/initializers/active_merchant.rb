d19eefabcb175e9a8e0dbacc1d7001a37f26c65c
Thomas <thomasmaximini@gmx.de>
2011-08-02 23:03:48 +0200

    fixed bugs, double validations removed

--- config/initializers/active_merchant.rb
+++ config/initializers/active_merchant.rb
@@ -1,2 +1,5 @@
-ActiveMerchant::Billing::PaypalGateway.logger = Logger.new(STDOUT) 
-ActiveMerchant::Billing::PaypalGateway.wiredump_device = File.new(File.join([Rails.root, "log", "paypal.log"]), "a")
+# ONLY for development - remember to turn OFF logging in production
+
+# writes the generated SOAP Requests to log/paypal.log
+# ActiveMerchant::Billing::PaypalGateway.logger = Logger.new(STDOUT) 
+# ActiveMerchant::Billing::PaypalGateway.wiredump_device = File.new(File.join([Rails.root, "log", "paypal.log"]), "a")
