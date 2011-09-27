a40e33efbf1a1dde5e5d6885a4bc19c6974741ed
Thomas <thomasmaximini@gmx.de>
2011-09-27 19:41:58 +0200

    size selectors and stock items

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,3 +1,26 @@
 ActiveAdmin.register Order do
+  index do
+    column :id
+    column :order_nr
+    column "Order Date", :created_at
+    column :last_name
+    column :first_name
+    column :street
+    column :zipcode
+    column :city
+    column "Articles" do |order|
+      div :class => "cart" do
+        link_to pluralize(order.cart.total_items, "item"), order
+      end  
+    end
+    column :ip_address
+    default_actions
+  end
   
+  filter :order_nr
+  filter :created_at, :label => "Order Date"
+  filter :zipcode
+  filter :city
+  filter :last_name
+  filter :ip_address
 end
