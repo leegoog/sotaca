99192c8635788ad18df357dce84a6ac5ff76f9da
Thomas <thomasmaximini@gmx.de>
2011-09-28 20:48:07 +0200

    resque heroku

--- config/environment.rb
+++ config/environment.rb
@@ -23,5 +23,8 @@ Money.default_currency = Money::Currency.new("GBP")
 # redis
 ENV["REDISTOGO_URL"] = "redis://sotaca:5a08419c13741bca5336e34605ddafcd@icefish.redistogo.com:9307/"
 
+uri = URI.parse(ENV["REDISTOGO_URL"])
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
 # Initialize the rails application
 Sotaca::Application.initialize!
