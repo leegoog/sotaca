02c2f475470b3ab35a7f2bb529c31ba24110151e
Thomas Maximini <thomas.maximini@medx.net>
2012-11-08 17:24:59 +0100

    translations

--- db/migrate/20110710084256_create_set_items.rb
+++ db/migrate/20110710084256_create_set_items.rb
@@ -1,5 +1,7 @@
 class CreateSetItems < ActiveRecord::Migration
   def self.up
+    drop_table :set_items
+    
     create_table :set_items do |t|
       t.integer :set_id
       t.integer :product_id
@@ -11,7 +13,6 @@ class CreateSetItems < ActiveRecord::Migration
 
       t.timestamps
     end
-    add_foreign_key(:set_items, :article_sets, :dependent => :delete_all)
   end
 
   def self.down
