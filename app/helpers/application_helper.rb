f7923b20d9796ff7b5970605c586b093a77f9483
Thomas Maximini <thomas.maximini@medx.net>
2012-11-19 17:00:50 +0100

    redcarpet compat

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
   
   def markdown(text)  
     options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis]  
-    Redcarpet.new(text, *options).to_html.html_safe  
+    Markdown.new(text, *options).to_html.html_safe  
   end
 
 
