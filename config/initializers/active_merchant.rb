7f3418e9ee2f1e825c32dcf5dbfb521d1b80e65a
Thomas Maximini <thomas.maximini@medx.net>
2012-11-19 11:28:19 +0100

    dump paypal logs

--- config/initializers/active_merchant.rb
+++ config/initializers/active_merchant.rb
@@ -1,5 +1,5 @@
 # ONLY for development - remember to turn OFF logging in production
 
 # writes the generated SOAP Requests to log/paypal.log
-# ActiveMerchant::Billing::PaypalGateway.logger = Logger.new(STDOUT) 
-# ActiveMerchant::Billing::PaypalGateway.wiredump_device = File.new(File.join([Rails.root, "log", "paypal.log"]), "a")
+ActiveMerchant::Billing::PaypalGateway.logger = Logger.new(STDOUT) 
+ActiveMerchant::Billing::PaypalGateway.wiredump_device = File.new(File.join([Rails.root, "log", "paypal.log"]), "a")
